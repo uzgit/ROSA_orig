b6fca6b36a93b6c9d78def8d7c6f42c6545661a3
Joshua <jspri17@gmail.com>
2018-12-03 15:45:06 +0100

    Initial commit.

---
+++ Debug/makedep.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+src\main.c
+
+src\kernel\rosa_int.c
+
+src\kernel\rosa_int_asm.S
+
+src\kernel\rosa_ker.c
+
+src\kernel\rosa_ker_asm.S
+
+src\kernel\rosa_scheduler.c
+
+src\kernel\rosa_tim.c
+
