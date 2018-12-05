4e5117fd62df409bd8a59f5b4c5051fb151d6eef
uzgit <jspri17@gmail.com>
2018-12-05 13:32:57 +0100

    Add files via upload

--- src/kernel/rosa_task.c
+++ src/kernel/rosa_task.c
@@ -1,4 +1,5 @@
 #include "include/kernel/rosa_task.h"
+#include "include/kernel/rosa_ker.h"
 
 //void ROSA_tcbCreate(tcb * tcbTask, char tcbName[NAMESIZE], void *tcbFunction, int * tcbStack, int tcbStackSize)
 
@@ -10,6 +11,12 @@ int16_t ROSA_taskCreate(ROSA_taskHandle_t * th, char * id, void * taskFunc, uint
 	
 	if( (task = calloc(1, sizeof(tcb))) && (stack = calloc(DEFAULT_STACK_SIZE, sizeof(int))) )
 	{
+		th = calloc(1, sizeof(tcb*));
+		task = calloc(1, sizeof(tcb));
+		stack = calloc(stackSize, sizeof(int));
+		
+		th = & task;
+		
 		task->priority			 = priority;
 		task->effective_priority = priority;
 		ROSA_tcbCreate(task, id, taskFunc, stack, stackSize);
@@ -21,11 +28,17 @@ int16_t ROSA_taskCreate(ROSA_taskHandle_t * th, char * id, void * taskFunc, uint
 	return result;
 }
 
-int16_t ROSA_taskDelete(ROSA_taskHandle_t tc)
+int16_t ROSA_taskDelete(ROSA_taskHandle_t th)
 {
 	int16_t result = -1;
 	
-	
+	if(th != 0)
+	{
+		free(th->dataarea);
+		free(th);
+		
+		result = 0;
+	}
 	
 	return result;
 }
